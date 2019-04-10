2229a5f0adc551b30ca2292cd880a0301c6d73b7
ShoichiYashiro <47899653+YaCpotato@users.noreply.github.com>
2019-04-10 14:03:32 +0900

    delete console.log for debug

--- porker_deck.js
+++ porker_deck.js
@@ -1,4 +1,3 @@
-
 var deck=new Vue({
     el:'#deck',
 
@@ -23,10 +22,6 @@ var deck=new Vue({
                 var num=Math.floor(Math.random()*this.Deck.length)//デッキのインデックスを乱数で
                 this.myDeck[i]=this.Deck[num]//該当インデックスの要素を手札に入れる
                 this.Deck.splice(num,1)//手札に入れたものをデッキから削除する
-                console.log('デッキの数'+this.Deck.length+'枚')//デッキが減ったことの確認
-            }
-            for(var i=0;i<5;i++){
-            console.log('手札'+(i+1)+'枚目'+this.myDeck[i])//手札の確認
             }
             this.desideSuit()
             this.isActive=!this.isActive
@@ -35,7 +30,6 @@ var deck=new Vue({
         
         //商、余をだす関数
         deckChange:function(event){//手札を変える
-            console.log('deckChange起動')
             //指定された添字のカードを再度乱数から数字を得る
             for(var i=0;i<5;i++){
                 if(deck.isChange[i]){
@@ -43,32 +37,20 @@ var deck=new Vue({
                     this.myDeck.splice(i,1,this.Deck[num])
                     this.Deck.splice(num,1)//手札に入れた$ものをデッキから削除する
                     this.mySuit.splice(i,1,this.suit[Math.floor(this.myDeck[i]/13)])
-                    //console.log(this.suit[this.Deck[num]])
                 }
             }
-            //this.desideSuit()
-            console.log('deckChange終了')
-
-            for(var i=0;i<5;i++){
-            console.log('手札'+(i+1)+'枚目'+this.myDeck[i])//手札の確認
-            }
-            console.log('デッキの数'+this.Deck.length+'枚')//デッキが減ったことの確認
         },
         desideSuit:function(){
             for(var i=0;i<5;i++){
                 var num=Math.floor(this.myDeck[i]/13)
                 if(num==0){
                     this.mySuit.push(this.suit[0])
-                    console.log((i+1)+"枚目のスートは"+this.suit[0])
                 }else if(num==1){
                     this.mySuit.push(this.suit[1])
-                    console.log((i+1)+"枚目のスートは"+this.suit[1])
                 }else if(num==2){
                     this.mySuit.push(this.suit[2])
-                    console.log((i+1)+"枚目のスートは"+this.suit[2])
                 }else if(num==3){
                     this.mySuit.push(this.suit[3])
-                    console.log((i+1)+"枚目のスートは"+this.suit[3])
                 }
             }
         }
--- role.js
+++ role.js
@@ -14,41 +14,29 @@ var role=new Vue({
     
     methods:{
         Flash:function(){//フラッシュ判定
-            console.log('Flash(),isActive.')
             var m=Math.floor(this.role_deck[0]/13)//自分のデッキの1枚目のカードの１３で割った商
-            //console.log(m)
             for(var i=1;i<5;i++){
-                //console.log(Math.floor(this.role_deck[i]/13))
                 if(m!=Math.floor(this.role_deck[i]/13)){
-                    console.log('Not Flash')
                     return 0;
                 }
             }
             this.Flash_judge=true
         },
+
         Straight:function(){
-            console.log('Straight(),isActive.')
             var i=0
             var surplus=[]
             for(var i=0;i<5;i++){
                 surplus.push(this.role_deck[i]%13)
             }
-//ソートする
             surplus.sort(
                 //これがないと、数字ではなく、文字列としてソートしてしまう/*
                 function(a,b){
                     return a-b;
             }
             )
-            for(var i=0;i<5;i++){
-                console.log(surplus[i])
-            }
-
-
             for(var i=4;i>=1;i--){
-                console.log('i='+i)
                 if(surplus[i]!=surplus[i-1]+1){
-                    console.log(i+'連番判定×')
                     return 0;
                 }
                 if(i==1){
@@ -57,29 +45,23 @@ var role=new Vue({
             }
             
             if(surplus[4]!=12 || surplus[0]!=0){
-                console.log('RS判定×')
                 return 0;
             }else if(surplus[4]==12 || surplus[0]==0){
                 this.Straight_judge=false
                 this.RoyalStraight_judge=true
             }
             if(surplus[4]-surplus[0]!=4){
-                //RoyalStraight時にはでる
-                console.log('前後判定×')
                 this.RoyalStraight_judge=false
                 return 0;
             }
         },
 
         FourCard:function(){//４カード・・・剰余が等しいカードが4枚
-            console.log('FourCard(),isActive.')
             var surplus=[]
             for(var i=0;i<5;i++){
                 surplus.push(this.role_deck[i]%13)
-                console.log('surplus[i]='+surplus[i])
             }
             surplus.sort(
-                //これがないと、数字ではなく、文字列としてソートしてしまう/*
                 function(a,b){
                     return a-b;
             }
@@ -89,27 +71,20 @@ var role=new Vue({
             for(var i=0;i<5;i++){
                 suit[surplus[i]]=suit[surplus[i]]+1
             }
-            console.log('suit= '+suit)
 
             for(var j=0;j<13;j++){
-                console.log('number check detected')
-                console.log(suit[j])
                 if(suit[j]==4){
-                    console.log('FourCard judge')
                     this.FourCard_judge=true
                 }
             }
         },
 
         FullHouse:function(){
-            console.log('FullHouse(),isActive.')
             var surplus=[]
             for(var i=0;i<5;i++){
                 surplus.push(this.role_deck[i]%13)
-                console.log('surplus[i]='+surplus[i])
             }
             surplus.sort(
-                //これがないと、数字ではなく、文字列としてソートしてしまう/*
                 function(a,b){
                     return a-b;
             }
@@ -124,7 +99,6 @@ var role=new Vue({
                             }
                         this.FullHouse_judge=true//等しかったら、昇順に並べたときに、3枚、2枚となるフルハウス
                     }else if(surplus[i+1]!=surplus[i+2]){
-                        console.log('Not a 3-2 FullHouse,then...')
                         i=2
                     }
                 }
@@ -132,11 +106,9 @@ var role=new Vue({
             this.FullHouse_judge=true
         },
         ThreeCard:function(){
-            console.log('ThreeCard(),isActive.')
             var surplus=[]
             for(var i=0;i<5;i++){
                 surplus.push(this.role_deck[i]%13)
-                console.log('surplus[i]='+surplus[i])
             }
             surplus.sort(
                 //これがないと、数字ではなく、文字列としてソートしてしまう/*
@@ -149,24 +121,18 @@ var role=new Vue({
             for(var i=0;i<5;i++){
                 suit[surplus[i]]=suit[surplus[i]]+1
             }
-            console.log('suit= '+suit)
 
             var pair=0
             for(var j=0;j<13;j++){
-                console.log('number check detected')
-                console.log(suit[j])
                 if(suit[j]==3){
-                    console.log('threeCard judge')
                     this.ThreeCard_judge=true
                 }
             }
         },
         Pair:function(){
-            console.log('Pair(),isActive.')
             var surplus=[]
             for(var i=0;i<5;i++){
                 surplus.push(this.role_deck[i]%13)
-                console.log('surplus[i]='+surplus[i])
             }
             surplus.sort(
                 //これがないと、数字ではなく、文字列としてソートしてしまう/*
@@ -179,25 +145,17 @@ var role=new Vue({
             for(var i=0;i<5;i++){
                 suit[surplus[i]]=suit[surplus[i]]+1
             }
-            console.log('suit= '+suit)
 
             var pair=0
             for(var j=0;j<13;j++){
-                console.log('number check detected')
-                console.log(suit[j])
                 if(suit[j]==2){
-                    console.log('pair judge')
                     pair=pair+1
-                    console.log('pair='+pair)
                 }
             }
-            console.log('最終確認pair='+pair)
             if(pair==2){
-                console.log('最終確認ツーペアpair='+pair)
                 this.TwoPair_judge=true
                 return 0;
             }else if(pair==1){
-                console.log('最終確認ワンペアpair='+pair)
                 this.TwoPair_judge=false
                 this.OnePair_judge=true
                 return 0;
@@ -213,34 +171,12 @@ var role=new Vue({
             this.ThreeCard_judge=false
             this.TwoPair_judge=false
             this.OnePair_judge=false
-            console.log('AllRoleJudge begin')
             this.Flash()
             this.Straight()
             this.FourCard()
             this.FullHouse()
             this.ThreeCard()
             this.Pair()
-            console.log('AllRoleJudge,isActive.')
-            if(this.Straight_judge){
-                return console.log('Straight!!!');   
-            }
-            if(this.RoyalStraight_judge && this.Flash_judge){
-                return console.log('Royal Straight Flash!!!');
-            }else if(this.RoyalStraight_judge){
-                console.log('Royal Straight!!!')
-            }else if(this.Flash_judge){
-                return console.log('Flash!!!');
-            }else if(this.FourCard_judge){
-                return console.log('FourCard!!!')
-            }else if(this.FullHouse_judge){
-                return console.log('FullHouse!!!')
-            }else if(this.ThreeCard_judge){
-                return console.log('ThreeCard!!!')
-            }else if(this.TwoPair_judge){
-                return console.log('Two Pair!!!')
-            }else if(this.OnePair_judge){
-                return console.log('One Pair!!!')
-            }
         }
     }
 })
